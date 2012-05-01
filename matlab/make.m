8825995efdcd3bb28e8aeca9183732df6d975c99
coha <coha@16e7d947-dcc2-db11-b54a-0017319806e7>
2012-05-01 08:19:50 +0000

    Modify the make.m of matlab, the line after else should be moved right.

--- matlab/make.m
+++ matlab/make.m
@@ -11,7 +11,7 @@ try
 	% This part is for MATLAB
 	% Add -largeArrayDims on 64-bit machines of MATLAB
 	else
-	mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmread.c
+	  mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmread.c
 		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmwrite.c
 		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims train.c linear_model_matlab.c ../linear.cpp ../tron.cpp "../blas/*.c"
 		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims predict.c linear_model_matlab.c ../linear.cpp ../tron.cpp "../blas/*.c"
